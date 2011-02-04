8e37b2e07721eae5a50aeefa6573c68f7f709f3e
Morgan Prior <morgan@amaras-tech.co.uk>
2011-02-04 23:35:24 +0000

    corrected File indentation. (2 spaces) And use File.join for concatenating paths, more reliable handling of trailing /

--- lib/rfuse_symbolic_fs.rb
+++ lib/rfuse_symbolic_fs.rb
@@ -4,85 +4,84 @@ require 'rfuse_symbolic_fs_opts'
 
 
 class RFuseSymbolicFS
-   # contents( path )
-   # file?( path )
-   # directory?( path )
-   # read_file( path )
-   # size( path )
-   # 
-   # save
-   # touch( path )
-   # can_write?(path)
-   # write_to(path,body)
-   # 
-   # can_delete?(path)
-   # delete( path )
-   #
-   # can_mkdir?( path )
-   # mkdir( path )
-   # can_rmdir( path )
-   # rmdir( path )
-   # 
+  # http://rubydoc.info/gems/fusefs/0.7.0/FuseFS/MetaDir
+  # contents( path )
+  # file?( path )
+  # directory?( path )
+  # read_file( path )
+  # size( path )
+  # 
+  # save
+  # touch( path )
+  # can_write?(path)
+  # write_to(path,body)
+  # 
+  # can_delete?(path)
+  # delete( path )
+  #
+  # can_mkdir?( path )
+  # mkdir( path )
+  # can_rmdir( path )
+  # rmdir( path )
+  # 
+
+  def initialize( options )
+    @base_dir = options.input
+  end
+
+  def contents(path)
+    n_path = File.expand_path( File.join(@base_dir, path ) )
+    Dir.chdir(n_path)  
+
+    files = Dir.glob('*')
+    #Added command to OS X Finder not to index.
+    files << 'metadata_never_index'
+
+    return files
+  end
+
+  def file?(path)
+    #If path ends with metadata_never_index it is a file
+    if path =~ /metadata_never_index$/
+      return true
+    end
+
+    return (not File.directory?( File.join( @base_dir, path ) ) )
+  end
+
+  def directory?(path)
+    File.directory?( File.join( @base_dir, path ) )
+  end
+
+  def read_file(path)
+
+    #puts "read file #{path}"
+    if File.exists?( File.join( @base_dir, path ) )
+      return File.new( File.join( @base_dir, path ) , "r").read
+    end
+    return "ERROR, file not found\n"
+
+  end
 
 
-
-   def initialize( options )
-      @base_dir = options.input
-   end
-
-   def contents(path)
-      n_path = File.expand_path( @base_dir + path )
-      Dir.chdir(n_path)  
-
-      files = Dir.glob('*')
-      #Added command to OS X Finder not to index.
-      files << 'metadata_never_index'
-
-      return files
-   end
-  
-   def file?(path)
-      #If path ends with metadata_never_index it is a file
-      if path =~ /metadata_never_index$/
-         return true
-      end
-
-      return (not File.directory?( @base_dir + path ))
-   end
-
-   def directory?(path)
-      File.directory?(@base_dir + path)
-   end
-  
-   def read_file(path)
-      
-      #puts "read file #{path}"
-      if File.exists?( @base_dir + path )
-         return File.new(@base_dir + path , "r").read
-      end
-      return "ERROR, file not found\n"
-
-   end
-
-
-   def size(path)
-      if File.exists?( @base_dir + path )
-         return File.size( @base_dir + path )
-      else
-         return 16
-      end
-   end
+  def size(path)
+    if File.exists?( File.join( @base_dir, path ) )
+      return File.size( File.join( @base_dir, path ) )
+    else
+      return 16
+    end
+  end
 end
 
 
 if $0 == __FILE__
 
-   options = RFuseSymbolicFSOpts.parse(ARGV)
-   filesystem = RFuseSymbolicFS.new( options )
-   FuseFS.set_root( filesystem )
+  options = RFuseSymbolicFSOpts.parse(ARGV)
+  filesystem = RFuseSymbolicFS.new( options )
+  FuseFS.set_root( filesystem )
 
-   # Mount under a directory given on the command line.
-   FuseFS.mount_under options.mountpoint
-   FuseFS.run
+  # Mount under a directory given on the command line.
+  FuseFS.mount_under options.mountpoint
+  FuseFS.run
 end
 
